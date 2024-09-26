89389148f6958e61bed860a9bb6ce138a1296012
Dev-Narpheesah <159795729+Dev-Narpheesah@users.noreply.github.com>
2024-09-26 22:11:36 +0100

    test

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,7 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
 
-// Import your components
+
 import Home from "./Component/Home/Home";
 import SignIn from "./Component/SignIn/SignIn";
 import Register from './Component/Register/Register';
